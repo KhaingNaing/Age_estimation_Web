369d52f3aa2600d61ef4e2c294dff4cea4c2ebf6
KhaingNaing <kmyatnoe7889@gmail.com>
2024-08-01 00:37:13 +1000

    Add Github page deployment config

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
+import { Container, Typography, Button, Box } from '@mui/material';
+import CircularWithValueLabel from './components/CircularWithValueLabel';
+import MenuAppBar from './components/MenuAppBar';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'; // Import CheckCircleIcon from the same package
+
+import { styled } from '@mui/system';
+import SendIcon from '@mui/icons-material/Send';
+import './App.css'
+
+const Input = styled('input')({
+  display: 'none',
+});
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [age, setAge] = useState(null);
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [uploaded, setUploaded] = useState(null);
 
   const handleFileChange = (event) => {
+    setUploaded(true);
     setSelectedFile(event.target.files[0]);
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!selectedFile) return;
+
+    setLoading(true);
 
     const formData = new FormData();
     formData.append('file', selectedFile);
@@ -32,17 +50,109 @@ function App() {
     } catch (error) {
       console.error('Error:', error);
     }
+    setLoading(false);
+    setUploaded(false);
   }
   return (
-    <div className='App'>
-      <h1>Age Estimation</h1>
+    <Box sx={{ position: 'relative', height: '100vh', width: '100%'}}>
+      {/* <h1>Age Estimation</h1>
       <form onSubmit={handleSubmit}>
         <input type='file' onChange={handleFileChange} />
         <button type='submit'>Predict Age</button>
-      </form>
-      {image && <img src={image} alt='age_image' />}
-      {age && <p>You are {parseFloat(age).toFixed(1)} years old!</p>}
-    </div>
+      </form> */}
+      {/* Fixed Container for Background Image */}
+      <Box
+        sx={{
+          position: 'relative',
+          height: '40%',
+          width: '100%',
+          overflow: 'hidden', // Ensures zoomed image doesn't overflow the container
+          boxSizing: 'border-box',
+        }}
+      >
+        <Box
+          component="img"
+          src={'/background-pic.jpg'}
+          alt="Background"
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover', // Ensures the image covers the container
+            animation: 'zoomIn 10s ease-in-out infinite',
+            boxSizing: 'border-box',
+          }}
+        />
+        {/* Header */}
+        {/* <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            padding: 1.5,
+            backgroundColor: 'rgba(0, 0, 0, 0)', // Semi-transparent background
+            color: 'white',
+            zIndex: 1, // Ensures the header is on top of the image
+            boxSizing: 'border-box',
+          }}
+        >
+          <Typography fontFamily='sans-serif'>Age Estimation</Typography>
+        </Box> */}
+        <MenuAppBar />
+      </Box>
+      <Box sx={{ my: 4, textAlign: 'center' }}>
+        <Typography variant='h3' component='h1' fontFamily='sans-serif' gutterBottom>
+          Age Estimation
+        </Typography>
+        <Typography variant='body' component='body' fontFamily='sans-serif' sx={{mx:4}} gutterBottom>
+        Try Out Our Age Prediction Model! <br/>
+
+Upload a selfie to see how our simple CNN model estimates your age. This model has around 6,000 parameters and provides quick predictions based on your uploaded image.        </Typography>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='upload-photo'>
+            <Input
+              accept='image/*'
+              id='upload-photo'
+              type='file'
+              onChange={handleFileChange} 
+            />
+            <Button
+              variant="contained"
+              color={uploaded ? "success" : "primary"}
+              component="span"
+              startIcon={uploaded ? <CheckCircleIcon /> : <CloudUploadIcon />}
+              sx={{ margin: 1 }}
+            >
+              {uploaded ? 'Uploaded' : 'Upload'}
+            </Button>
+          </label>
+          <Box sx={{ mt: 2 }}>
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<SendIcon />}
+              sx={{ margin: 1 }}
+              type='submit'
+              disabled={loading}
+            >
+              Predict Age
+            </Button>
+          </Box>
+        </form>
+      </Box>
+
+      {loading && <CircularWithValueLabel sx={{ mt: 2 }} />}
+      {/* {image && <img src={image} alt='age_image' />} */}
+      {/* {age && <p>It looks like you are {parseFloat(age).toFixed(1)} years old!</p>} */}
+      { age && 
+        <Box sx={{ mt: 4, textAlign: 'center'}}>
+          <img src={image} alt='age-image' style={{ maxWidth: '100%' }}/>
+          <Typography>It looks like you are {parseFloat(age).toFixed(1)} years old!</Typography>
+        </Box>}
+    </Box>
   )
 }
 
---
+++ src/components/MenuAppBar.js
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
+export default function MenuAppBar() {
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [active, setActive] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openProfile = () => {
+    window.open('https://github.com/KhaingNaing', '_blank');
+  }
+
+  const handleGitHubClick = () => {
+    setActive(true);
+    window.open('https://github.com/KhaingNaing/Age_estimation_CV', '_blank');
+    setTimeout(() => setActive(false), 2000); // Reset active state after 2 seconds
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <Typography variant="h7" component="div" sx={{ flexGrow: 1 }}>
+            AGE PREDICT
+          </Typography> */}
+
+          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+            <Typography variant="h6" component="div" sx={{ marginRight: 2 }}>
+              AgePredict
+            </Typography>
+            <Typography
+              variant="h6"
+              component="div"
+              onClick={handleGitHubClick}
+              sx={{
+                cursor: 'pointer',
+                position: 'relative',
+                padding: '4px 8px',
+                borderRadius: '4px',
+                transition: 'background-color 0.3s, color 0.3s',
+                backgroundColor: active ? '#d0d0d0' : 'transparent',
+                color: active ? '#000' : 'inherit',
+                '&:hover': {
+                  backgroundColor: '#f0f0f0',
+                  color: '#000',
+                },
+              }}
+            >            
+            GitHubCode
+            </Typography>
+          </Box>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                sx={{
+                  padding: 0
+                }}
+              >
+                <img
+                  src="/account.jpg" 
+                  alt="Account"
+                  style={{ width: 30, height: 30, objectFit: 'cover', borderRadius: '50%'}} // Adjust size as needed
+                />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={openProfile}>Profile</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
